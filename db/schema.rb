3facd767411ee23eb9c68d66aa82c128954f66f9
John Elder <johne4196@gmail.com>
2016-10-14 16:40:37 +0000

    added first, last, and username and goal fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014163007) do
+ActiveRecord::Schema.define(version: 20161014163359) do
 
   create_table "stats", force: :cascade do |t|
     t.integer  "miles_walked"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 20161014163007) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "user_name"
+    t.integer  "yearly_goal"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
