d070e9b7fbcbd3e59b29cee7046e8c042e0c29b9
John Elder <johne4196@gmail.com>
2016-10-15 02:29:19 +0000

    fixed has_many association name convention

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @stats = Stat.all
     @john_stats = User.find_by(id: 1)
-    @elder_stats = @john_stats.stat
+    @elder_stats = @john_stats.stats
     
   end
 end
